6da722387bef4fcde6c0132e3a437bf40c03d614
Salim Chibani <salimshibani5@gmail.com>
2024-04-15 22:06:31 +0100

    first commit

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { LoginModule } from './login/login.module'; // Import LoginModule here
+import { LoginModule } from './login/login.module'; 
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
     imports: [
       MongooseModule.forRoot(process.env.MONGO_URI),
-        LoginModule, // Include LoginModule here
+        LoginModule, 
     ],
     controllers: [AuthController],
     providers: [AuthService],
